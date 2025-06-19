87dfa3ae47e7596af87c7b7613ec6d32b72a6b23
Ninh Hải Đăng <93443383+ninhhaidang@users.noreply.github.com>
2025-06-19 21:07:46 +0700

    Delete vi_du_don_gian directory

--- vi_du_don_gian/can_bac_2.py
+++
@@ -1,6 +0,0 @@
-import math
-
-a = 16
-result = math.sqrt(a)
-print(f"Căn bậc hai của {a} là: {result}")
-# Kết quả: Căn bậc hai của 16 là: 4.0
--- vi_du_don_gian/luy_thua.py
+++
@@ -1,7 +0,0 @@
-import math
-
-a = 16
-
-result = math.pow(a, 2)
-print(f"Lũy thừa của {a} là: {result}")
-# Kết quả: Lũy thừa của 16 là: 256.0
